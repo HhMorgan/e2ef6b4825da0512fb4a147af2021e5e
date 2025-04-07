d757dc8d5c8931bad4cb9e31694d22a10d714845
RiediJohannes <johannes.riedmann.vlbg@gmail.com>
2025-04-07 19:07:08 +0200

    Ex 1.2 - Probably solution

--- src/ex_1/ex1.2.py
+++ src/ex_1/ex1.2.py
@@ -36,32 +36,57 @@ def read_instance_file(filename: str | os.PathLike) -> tuple[np.ndarray, np.ndar
 
 def build_model(model: gp.Model, processing_times: np.ndarray, machine_sequences: np.ndarray):
     # note that both jobs and machines are 0-indexed here
-    n_jobs, n_machines = processing_times.shape
+    n, m = processing_times.shape
 
-    # put your model building code here
-    #
-    # x = model.addVars(...)
-    #
-    # if you want to access your variables outside this function, you can use
-    # model._x = x
-    # to save a reference in the model itself
-    #
-    # model.addConstrs(...)
+    p = list(processing_times)
+    pie = list(machine_sequences)
+
+    x = model.addVars(
+        [(i,j,k) for i in range(m) for j in range(n) for k in range(n)],
+        name="x",
+        vtype=GRB.BINARY,
+    )
+
+    s = model.addVars(
+        [(i,j) for i in range(m) for j in range(n)],
+        name="s",
+        vtype=GRB.INTEGER,
+    )
+
+    model.addConstrs((s[i,j] >= s[i,k] + p[k][i] - GRB.MAXINT * (1 - x[i,k,j]) for i in range(m) for j in range(n) for k in range(n)),
+                     name="A job j can only start after its preceding job k finished")
+    #model.addConstrs((s[i,j] + p[j][i] <= s[i,k] + GRB.MAXINT * (1 - x[i,j,k]) for i in range(m) for j in range(n) for k in range(n)),
+     #                name="A job j must start before its succeeding job k is started")
+
+    model.addConstrs((gp.quicksum(x[i,j,k] for j in range(n) for k in range(n) if j != k) * 2 == n*(n-1) for i in range(m)),
+                     name="On every machine, the sum of job precedence must be n(n-1)/2")
+
+    model.addConstrs((s[pie[j][l],j] >= s[pie[j][l-1],j] + p[j][pie[j][l-1]] for l in range(1, m) for j in range(n)),
+                     name="Enforce the correct order of machines for job j")
+
+
+    # we want to get the sum of the job completion times of the last job on the last machine
+    model.setObjective(gp.quicksum(s[pie[j][m-1],j] + p[j][pie[j][m-1]] for j in range(n)), GRB.MINIMIZE)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--filename", default="instances/ex1.2-instance.dat")
+    parser.add_argument("--filename", default="../../instances/ex1.2-instance.dat")
     args = parser.parse_args()
 
     processing_times, machine_sequences = read_instance_file(args.filename)
-    n_jobs, n_machines = processing_times.shape
+    # n_jobs, n_machines = processing_times.shape
 
     model = gp.Model("ex1.2")
     build_model(model, processing_times, machine_sequences)
 
     model.update()
     model.optimize()
+
+    if model.status == GRB.INF_OR_UNBD or model.status == GRB.INFEASIBLE:
+        print("Model is infeasible. Computing IIS...")
+        model.computeIIS()
+        model.write("model.ilp")
 
     if model.SolCount > 0:
         print(f"obj. value = {model.ObjVal}")
