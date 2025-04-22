948eb7a3d2b2e0186e3c33560595a38aebdc75b2
RiediJohannes <johannes.riedmann.vlbg@gmail.com>
2025-04-22 07:23:34 +0200

    Project - Added (incomplete) formulations for SCF and MCF

--- src/project/part_1/model.py
+++ src/project/part_1/model.py
@@ -53,9 +53,8 @@ def create_model(model: gp.Model, graph: nx.Graph, k: int):
     # create common variables
     # see, e.g., https://docs.gurobi.com/projects/optimizer/en/current/reference/python/model.html#Model.addVars
 
-    # x = m.addVars(...)
     x = model.addVars(
-        [(i, j) for (i, j) in present_edges],
+        present_edges,
         name="x",
         vtype=GRB.BINARY
     )
@@ -76,21 +75,70 @@ def create_model(model: gp.Model, graph: nx.Graph, k: int):
             vtype=GRB.INTEGER,
         )
 
-
+        # TODO Think about how to change the x_e variable constraint to a directed formulation
         model.addConstrs((v[i] >= 1 for i in node_indices), "positive ordering")
         model.addConstrs((v[i] <= node_indices[-1] for i in node_indices), "integer-step ordering")
         model.addConstr(v[0] == 0, "zero vertex as root node")
         model.addConstr(gp.quicksum(x[i,j] for (i,j) in present_edges) == k, "MST with k vertices")
 
-
     elif model._formulation == "scf":
-        pass
+        f = model.addVars(
+            present_edges,
+            name="f",
+            vtype=GRB.INTEGER,
+        )
+
+        model.addConstr(gp.quicksum(f[0, j] for j in node_indices) == k, name="source flow")
+
+        model.addConstrs((gp.quicksum(f[i, j] for (i, j) in present_edges) -
+                          gp.quicksum(f[j, i] for (j, i) in present_edges) == 1
+                          for j in node_indices),
+                          name="source flow")
+
+        model.addConstrs((0 <= f[i,j] for (i, j) in present_edges), name="positive flow")
+        model.addConstrs((f[i,j] <= (k-1) * x[i,j] for (i, j) in present_edges), name="capped flow")
+
     elif model._formulation == "mcf":
-        pass
+        present_edges_times_vertices = [(i,j,v) for (i,j) in present_edges for v in node_indices]
+
+        f = model.addVars(
+            present_edges_times_vertices,
+            name="f",
+            vtype=GRB.BINARY,
+        )
+
+        model.addConstrs((gp.quicksum(f[0, j, v] for j in node_indices) == 1
+                        for v in node_indices), name="source flow")
+
+        # does not work
+        # for v in node_indices:
+        #     incoming_flow = 0
+        #     for (i,j) in present_edges:
+        #         if j == v:
+        #             incoming_flow += f[i,j,v]
+        #
+        #     model.addConstr(incoming_flow == x[i,v], name="consume own unit flow")
+
+        model.addConstrs((gp.quicksum(f[i,j,v] for (i,j,v) in present_edges_times_vertices if j == j_n and v == v_n) -
+                          gp.quicksum(f[j,i,v] for (i,j,v) in present_edges_times_vertices if j == j_n and v == v_n)
+                          == 0
+                          for j_n in node_indices for v_n in node_indices),
+                         name="non-consumption of foreign flow")
+
+        model.addConstrs((0 <= f[i,j,v] for (i,j,v) in present_edges_times_vertices),
+                         name="positive flow")
+        model.addConstrs((f[i,j,v] <= x[i,j] for (i,j,v) in present_edges_times_vertices),
+                         name="unit flow")
+        model.addConstr(gp.quicksum(x[i,j] for (i,j) in present_edges) == k-1,
+                        name="take k-1 edges")
+
     elif model._formulation == "cec":
         pass
     elif model._formulation == "dcc":
         pass
+
+    # common objective function
+    model.setObjective(gp.quicksum(x[i,j] * graph.edges[i,j]['cost'] for (i,j) in present_edges), GRB.MINIMIZE)
 
 def get_selected_edge_ids(model: gp.Model) -> list[int]:
     # note that you may need to account for tolerances
