98fae617cebcba5f38c2100c62ec091b2157489e
RiediJohannes <johannes.riedmann.vlbg@gmail.com>
2025-04-28 21:38:06 +0200

    Project - Renamed present_edges to arcs

--- src/project/part_1/model.py
+++ src/project/part_1/model.py
@@ -48,13 +48,13 @@ def create_model(model: gp.Model, graph: nx.Graph, k: int):
 
     node_indices = [n for n in graph] # grab the ID of every node in the graph
 
-    reversed_edges = {(j, i) for (i, j) in graph.edges}
-    present_edges = reversed_edges.union(set(graph.edges))  # edges in graph and their inverted counterpart
-    present_edges_with_root = present_edges.union((0, j) for j in node_indices)
+    reversed_arcs = {(j, i) for (i, j) in graph.edges}
+    arcs = reversed_arcs.union(set(graph.edges))  # edges in graph and their inverted counterpart
+    arcs_with_zero = arcs.union((0, j) for j in node_indices)
 
     # common variables
     x = model.addVars(
-        present_edges_with_root,
+        arcs_with_zero,
         name="x",
         vtype=GRB.BINARY
     )
@@ -76,12 +76,12 @@ def create_model(model: gp.Model, graph: nx.Graph, k: int):
         )
 
         model.addConstrs((v[i] + x[i,j] <= v[j] + k * (1 - x[i,j])
-                          for (i,j) in present_edges_with_root),
+                          for (i,j) in arcs_with_zero),
                           "impose order of connected vertices")
-        model.addConstrs((gp.quicksum(x[i,j] for (i,j) in present_edges_with_root if j == j_g) == y[j_g]
+        model.addConstrs((gp.quicksum(x[i,j] for (i,j) in arcs_with_zero if j == j_g) == y[j_g]
                           for j_g in node_indices),
                          "only one incoming edge")
-        model.addConstrs((y[i] + y[j] >= 2 * x[i, j] for (i, j) in present_edges),
+        model.addConstrs((y[i] + y[j] >= 2 * x[i, j] for (i, j) in arcs),
                          "edge implies vertices")
 
         # constraints for root node 0
@@ -89,7 +89,7 @@ def create_model(model: gp.Model, graph: nx.Graph, k: int):
         model.addConstr(gp.quicksum(x[0,j] for j in node_indices) == 1,
                         "one vertex as root node")
 
-        model.addConstr(gp.quicksum(x[i,j] for (i,j) in present_edges) == k - 1,
+        model.addConstr(gp.quicksum(x[i,j] for (i,j) in arcs) == k - 1,
                         "MST with k-1 edges")
         # define restricted interval for order variable
         model.addConstrs((v[i] >= y[i] for i in node_indices),
@@ -106,7 +106,7 @@ def create_model(model: gp.Model, graph: nx.Graph, k: int):
         )
 
         f = model.addVars(
-            present_edges_with_root,
+            arcs_with_zero,
             name="f",
             vtype=GRB.INTEGER,
         )
@@ -114,32 +114,32 @@ def create_model(model: gp.Model, graph: nx.Graph, k: int):
         model.addConstr(gp.quicksum(f[0, j] for j in node_indices) == k, name="source flow")
         model.addConstr(gp.quicksum(x[0, j] for j in node_indices) == 1, name="only one edge from root")
 
-        model.addConstrs((gp.quicksum(f[i, j] for (i, j) in present_edges_with_root if j == j_n) -
-                          gp.quicksum(f[j, i] for (j, i) in present_edges_with_root if j == j_n) == y[j_n]
+        model.addConstrs((gp.quicksum(f[i, j] for (i, j) in arcs_with_zero if j == j_n) -
+                          gp.quicksum(f[j, i] for (j, i) in arcs_with_zero if j == j_n) == y[j_n]
                           for j_n in node_indices),
                           name="consume one unit")
 
-        model.addConstrs((y[n] >= 1/len(node_indices) * gp.quicksum(x[i,j] for (i,j) in present_edges_with_root if j == n)
+        model.addConstrs((y[n] >= 1/len(node_indices) * gp.quicksum(x[i,j] for (i,j) in arcs_with_zero if j == n)
                           for n in node_indices),
                           name="be present if you receive flow")
-        model.addConstrs((y[n] <= gp.quicksum(x[i,j] for (i,j) in present_edges_with_root if j == n)
+        model.addConstrs((y[n] <= gp.quicksum(x[i,j] for (i,j) in arcs_with_zero if j == n)
                           for n in node_indices),
                           name="be absent if you receive no flow")
 
         model.addConstrs((0 <= f[i,j]
-                          for (i, j) in present_edges_with_root),
+                          for (i, j) in arcs_with_zero),
                           name="positive flow")
         model.addConstrs((f[i,j] <= k * x[i,j]
-                          for (i, j) in present_edges_with_root),
+                          for (i, j) in arcs_with_zero),
                          name="capped flow")
 
 
     elif model._formulation == "mcf":
-        present_edges_times_vertices = set((i,j,v) for (i,j) in present_edges for v in node_indices)
-        edges_times_vertices_with_root = present_edges_times_vertices.union((0,j,v) for j in node_indices for v in node_indices)
+        arcs_times_vertices = set((i,j,v) for (i,j) in arcs for v in node_indices)
+        arcs_times_vertices_with_zero = arcs_times_vertices.union((0,j,v) for j in node_indices for v in node_indices)
 
         f = model.addVars(
-            edges_times_vertices_with_root,
+            arcs_times_vertices_with_zero,
             name="f",
             vtype=GRB.BINARY,
         )
@@ -148,25 +148,25 @@ def create_model(model: gp.Model, graph: nx.Graph, k: int):
                         for v in node_indices), name="source flow")
         model.addConstr(gp.quicksum(x[0, j] for j in node_indices) == 1, name="only one entry point")
 
-        model.addConstrs((gp.quicksum(f[i,v,v] for (i,v) in present_edges_with_root if v == v_n) -
-                          gp.quicksum(f[v,i,v] for (v,i) in present_edges_with_root if v == v_n)>=
-                         1/len(node_indices) * gp.quicksum(x[i,v] for (i,v) in present_edges_with_root if v == v_n)
+        model.addConstrs((gp.quicksum(f[i,v,v] for (i,v) in arcs_with_zero if v == v_n) -
+                          gp.quicksum(f[v,i,v] for (v,i) in arcs_with_zero if v == v_n)>=
+                         1/len(node_indices) * gp.quicksum(x[i,v] for (i,v) in arcs_with_zero if v == v_n)
                          for v_n in node_indices), "consume own flow")
 
-        model.addConstrs((gp.quicksum(f[i,j,v] for (i,j,v) in edges_times_vertices_with_root if j == j_n and v == v_n) -
-                          gp.quicksum(f[j,i,v] for (j,i,v) in edges_times_vertices_with_root if j == j_n and v == v_n)
+        model.addConstrs((gp.quicksum(f[i,j,v] for (i,j,v) in arcs_times_vertices_with_zero if j == j_n and v == v_n) -
+                          gp.quicksum(f[j,i,v] for (j,i,v) in arcs_times_vertices_with_zero if j == j_n and v == v_n)
                           == 0
                           for j_n in node_indices for v_n in node_indices if v_n != j_n),
                          name="non-consumption of foreign flow")
 
         model.addConstrs((0 <= f[i,j,v]
-                          for (i,j,v) in edges_times_vertices_with_root),
+                          for (i,j,v) in arcs_times_vertices_with_zero),
                           name="positive flow")
         model.addConstrs((f[i,j,v] <= x[i,j]
-                          for (i,j,v) in edges_times_vertices_with_root),
+                          for (i,j,v) in arcs_times_vertices_with_zero),
                           name="unit flow")
 
-        model.addConstr(gp.quicksum(x[i,j] for (i,j) in present_edges) == k-1,
+        model.addConstr(gp.quicksum(x[i,j] for (i,j) in arcs) == k-1,
                         name="take k-1 edges")
 
         model.addConstrs((x[i,j] + x[j,i] <= 1
@@ -180,7 +180,7 @@ def create_model(model: gp.Model, graph: nx.Graph, k: int):
         pass
 
     # common objective function
-    model.setObjective(gp.quicksum(x[i,j] * graph.edges[i,j]['cost'] for (i,j) in present_edges), GRB.MINIMIZE)
+    model.setObjective(gp.quicksum(x[i,j] * graph.edges[i,j]['cost'] for (i,j) in arcs), GRB.MINIMIZE)
 
 
 def get_selected_edge_ids(model: gp.Model, graph: nx.Graph) -> list[int]:
@@ -189,17 +189,15 @@ def get_selected_edge_ids(model: gp.Model, graph: nx.Graph) -> list[int]:
 
     # https://docs.gurobi.com/projects/optimizer/en/current/concepts/attributes/examples.html
 
-
-
-    reversed_edges = {(j, i) for (i, j) in graph.edges}
-    present_edges = reversed_edges.union(set(graph.edges))  # edges in graph and their inverted counterpart
+    reversed_arcs = {(j, i) for (i, j) in graph.edges}
+    arcs = reversed_arcs.union(set(graph.edges))  # edges in graph and their inverted counterpart
 
     selected_edges: list[int] = []
     if model.SolCount > 0:
         for v in sorted(model.getVars(), key=lambda x: x.VarName):
             print(f"{v.VarName:<8} = {v.X}")
 
-        for (i,j) in present_edges:
+        for (i,j) in arcs:
             x_ij = model.getVarByName(f'x[{i},{j}]')
 
             if x_ij.X == 1:
