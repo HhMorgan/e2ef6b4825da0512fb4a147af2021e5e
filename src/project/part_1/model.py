e5fc2d45617d09c868c18a7f0238ef532096bb90
RiediJohannes <johannes.riedmann.vlbg@gmail.com>
2025-06-02 01:11:19 +0200

    Added lots of TODOs for CEC and DCC formulations

--- src/project/part_1/model.py
+++ src/project/part_1/model.py
@@ -1,6 +1,6 @@
 import gurobipy as gp
-from gurobipy import GRB
 import networkx as nx
+from gurobipy import GRB
 
 
 def lazy_constraint_callback(model: gp.Model, where):
@@ -12,39 +12,61 @@ def lazy_constraint_callback(model: gp.Model, where):
         # get solution values for variables x
         # see https://docs.gurobi.com/projects/optimizer/en/current/reference/python/model.html#Model.cbGetSolution
 
-        # x_values = model.cbGetSolution(model._x)
-
         if model._formulation == "cec":
-            add_violated_cec(model)
+            inequality = find_violated_cec_int(model)
         elif model._formulation == "dcc":
-            add_violated_dcc(model)
+            inequality = find_violated_dcc_int(model)
 
     # check fractional solutions to find violated CECs/DCCs to strengthen the bound
     elif where == GRB.Callback.MIPNODE and model.cbGet(GRB.Callback.MIPNODE_STATUS) == GRB.OPTIMAL:
         # get solution values for variables x
         # see https://docs.gurobi.com/projects/optimizer/en/current/reference/python/model.html#Model.cbGetNodeRel
-        
-        # x_values = model.cbGetNodeRel(model._x)
 
         # you may also use different algorithms for integer and fractional separation if you want
         if model._formulation == "cec":
-            add_violated_cec(model)
+            inequality = find_violated_cec_float(model)
         elif model._formulation == "dcc":
-            add_violated_dcc(model)
+            inequality = find_violated_dcc_float(model)
+
+    # TODO In general: Add a cutting plane (strengthening inequality)
+    # x = model.cbGetNodeRel(model._x)
+    # rel = model.cbGetNodeRel(x)
+    # if rel[0] + rel[1] > 1.1:
+    #     model.cbCut(inequality)
+    #     model.cbCut(x[0] + x[1], GRB.LESS_EQUAL, 1)
 
 
-def add_violated_cec(model: gp.Model):
-    # add your CEC separation code here
+def find_violated_cec_int(model: gp.Model):
+    # TODO Create subgraph with chosen arcs {(i,j) | x_ij = 1}
+
+    # TODO Find a cycle in this subgraph and return a CEC for it
     pass
 
 
-def add_violated_dcc(model: gp.Model):
+def find_violated_dcc_int(model: gp.Model):
     # add your DCC separation code here
     pass
 
 
+def find_violated_cec_float(model: gp.Model):
+    # TODO Label all arcs in the digraph with weight w_ij = 1 - x_ij
+
+    # TODO For each arc (u,v), find cheapest path from v to u w.r.t weights w_ij (use networkx method like Dijkstra)
+    # for (i,j in ...:
+    # TODO If any shortest path plus arc (u,v) has total weight 0, create a cycle-elimination constraint from this
+    # return cec
+    pass
+
+
+def find_violated_dcc_float(model: gp.Model):
+    # TODO Something about finding a minimum cut (see slides) - use networkx mincut function for this
+
+    # return dcc
+    pass
+
+
 def create_model(model: gp.Model, graph: nx.Graph, k: int, *, digraph: nx.Graph = None):
-    node_indices = [n for n in graph] # grab the ID of every node in the graph
+    node_indices = [n for n in graph]  # grab the ID of every node in the graph
 
     reversed_arcs = {(j, i) for (i, j) in graph.edges}
     arcs = reversed_arcs.union(set(graph.edges))  # edges in graph and their inverted counterpart
@@ -61,6 +83,8 @@ def create_model(model: gp.Model, graph: nx.Graph, k: int, *, digraph: nx.Graph 
     digraph_with_zero.add_node(0)
     digraph_with_zero.add_edges_from((0, j) for j in node_indices)
 
+    model._graph = digraph  # add reference to the initial directed graph for use in CEC and DCC formulations
+
     # common variables
     x = model.addVars(
         arcs_with_zero,
@@ -74,7 +98,11 @@ def create_model(model: gp.Model, graph: nx.Graph, k: int, *, digraph: nx.Graph 
     )
 
     # add reference to relevant variables for later use in callbacks (CEC,DCC)
-    # model._x = x
+    model._x = x
+    model._y = y
+
+    # common constraints
+    model.addConstr(gp.quicksum(x[0, j] for j in node_indices) == 1, name="one_edge_from_root")
 
     # create model-specific variables and constraints
     if model._formulation == "seq":
@@ -84,10 +112,10 @@ def create_model(model: gp.Model, graph: nx.Graph, k: int, *, digraph: nx.Graph 
             vtype=GRB.CONTINUOUS,
         )
 
-        model.addConstrs((v[i] + x[i,j] <= v[j] + k * (1 - x[i,j])
-                          for (i,j) in arcs_with_zero),
-                          "impose_order")
-        model.addConstrs((gp.quicksum(x[i,j] for i in digraph_with_zero.predecessors(j)) == y[j]
+        model.addConstrs((v[i] + x[i, j] <= v[j] + k * (1 - x[i, j])
+                          for (i, j) in arcs_with_zero),
+                         "impose_order")
+        model.addConstrs((gp.quicksum(x[i, j] for i in digraph_with_zero.predecessors(j)) == y[j]
                           for j in node_indices),
                          "one_incoming_edge")
         model.addConstrs((y[i] + y[j] >= 2 * x[i, j] for (i, j) in arcs),
@@ -95,10 +123,8 @@ def create_model(model: gp.Model, graph: nx.Graph, k: int, *, digraph: nx.Graph 
 
         # constraints for root node 0
         model.addConstr(v[0] == 0, "zero_is_root")
-        model.addConstr(gp.quicksum(x[0,j] for j in node_indices) == 1,
-                        "one_entrypoint")
 
-        model.addConstr(gp.quicksum(x[i,j] for (i,j) in arcs) == k - 1,
+        model.addConstr(gp.quicksum(x[i, j] for (i, j) in arcs) == k - 1,
                         "k_vertices")
         # define restricted interval for order variable
         model.addConstrs((v[i] >= y[i] for i in node_indices),
@@ -116,29 +142,29 @@ def create_model(model: gp.Model, graph: nx.Graph, k: int, *, digraph: nx.Graph 
         )
 
         model.addConstr(gp.quicksum(f[0, j] for j in node_indices) == k, name="source_flow")
-        model.addConstr(gp.quicksum(x[0, j] for j in node_indices) == 1, name="one_edge_from_root")
 
         model.addConstrs((gp.quicksum(f[i, j] for i in digraph_with_zero.predecessors(j)) -
                           gp.quicksum(f[j, i] for i in digraph_with_zero.successors(j)) == y[j]
                           for j in node_indices),
-                          name="consume_one_unit")
+                         name="consume_one_unit")
 
-        model.addConstrs((y[j] >= 1/len(node_indices) * gp.quicksum(x[i,j] for i in digraph_with_zero.predecessors(j))
+        model.addConstrs(
+            (y[j] >= 1 / len(node_indices) * gp.quicksum(x[i, j] for i in digraph_with_zero.predecessors(j))
+             for j in node_indices),
+            name="flow_inclusion")
+        model.addConstrs((y[j] <= gp.quicksum(x[i, j] for i in digraph_with_zero.predecessors(j))
                           for j in node_indices),
-                          name="flow_inclusion")
-        model.addConstrs((y[j] <= gp.quicksum(x[i,j] for i in digraph_with_zero.predecessors(j))
-                          for j in node_indices),
-                          name="flow_exclusion")
+                         name="flow_exclusion")
 
         # hint: positive flow constraint is managed through lower bound for f variable
-        model.addConstrs((f[i,j] <= k * x[i,j]
+        model.addConstrs((f[i, j] <= k * x[i, j]
                           for (i, j) in arcs_with_zero),
                          name="capped_flow")
 
 
     elif model._formulation == "mcf":
-        arcs_times_vertices = set((i,j,v) for (i,j) in arcs for v in node_indices)
-        arcs_times_vertices_with_zero = arcs_times_vertices.union((0,j,v) for j in node_indices for v in node_indices)
+        arcs_times_vertices = set((i, j, v) for (i, j) in arcs for v in node_indices)
+        arcs_times_vertices_with_zero = arcs_times_vertices.union((0, j, v) for j in node_indices for v in node_indices)
 
         f = model.addVars(
             arcs_times_vertices_with_zero,
@@ -148,15 +174,14 @@ def create_model(model: gp.Model, graph: nx.Graph, k: int, *, digraph: nx.Graph 
         )
 
         model.addConstrs((gp.quicksum(f[0, j, v] for j in node_indices) <= y[v]
-                        for v in node_indices), name="source_flow")
-        model.addConstr(gp.quicksum(x[0, j] for j in node_indices) == 1, name="one_entry_point")
+                          for v in node_indices), name="source_flow")
 
-        model.addConstrs((gp.quicksum(f[i,v,v] for i in digraph_with_zero.predecessors(v)) -
-                          gp.quicksum(f[v,j,v] for j in digraph_with_zero.successors(v)) == y[v]
-                         for v in node_indices), "consume_own_flow")
+        model.addConstrs((gp.quicksum(f[i, v, v] for i in digraph_with_zero.predecessors(v)) -
+                          gp.quicksum(f[v, j, v] for j in digraph_with_zero.successors(v)) == y[v]
+                          for v in node_indices), "consume_own_flow")
 
-        model.addConstrs((gp.quicksum(f[i,j,v] for i in digraph_with_zero.predecessors(j)) -
-                          gp.quicksum(f[j,i,v] for i in digraph_with_zero.successors(j))
+        model.addConstrs((gp.quicksum(f[i, j, v] for i in digraph_with_zero.predecessors(j)) -
+                          gp.quicksum(f[j, i, v] for i in digraph_with_zero.successors(j))
                           == 0
                           for j in node_indices for v in node_indices if v != j),
                          name="non-consumption_foreign_flow")
@@ -164,26 +189,31 @@ def create_model(model: gp.Model, graph: nx.Graph, k: int, *, digraph: nx.Graph 
         model.addConstrs((y[i] + y[j] >= 2 * x[i, j] for (i, j) in arcs),
                          "edge_implies_vertices")
 
-        model.addConstrs((gp.quicksum(x[i,j] for i in digraph_with_zero.predecessors(j)) == y[j]
+        model.addConstrs((gp.quicksum(x[i, j] for i in digraph_with_zero.predecessors(j)) == y[j]
                           for j in node_indices),
                          "one_incoming_edge")
 
         # hint: positive flow constraint is managed through lower bound for f variable
-        model.addConstrs((f[i,j,v] <= x[i,j]
-                          for (i,j,v) in arcs_times_vertices_with_zero),
-                          name="unit_flow")
+        model.addConstrs((f[i, j, v] <= x[i, j]
+                          for (i, j, v) in arcs_times_vertices_with_zero),
+                         name="unit_flow")
 
-        model.addConstr(gp.quicksum(x[i,j] for (i,j) in arcs) == k-1,
+        model.addConstr(gp.quicksum(x[i, j] for (i, j) in arcs) == k - 1,
                         name="take_k_edges")
 
 
     elif model._formulation == "cec":
-        pass
+        # TODO Implement CEC formulation
+
+        model.addConstr(gp.quicksum(x[i, j] for (i, j) in arcs) == k - 1, "k_vertices")
+
     elif model._formulation == "dcc":
-        pass
+        # TODO Implement DCC formulation
+
+        model.addConstr(gp.quicksum(x[i, j] for (i, j) in arcs) == k - 1, "k_vertices")
 
     # common objective function
-    model.setObjective(gp.quicksum(x[i,j] * graph.edges[i,j]['cost'] for (i,j) in arcs), GRB.MINIMIZE)
+    model.setObjective(gp.quicksum(x[i, j] * graph.edges[i, j]['cost'] for (i, j) in arcs), GRB.MINIMIZE)
 
 
 def get_selected_edge_ids(model: gp.Model, graph: nx.Graph) -> list[int]:
@@ -200,11 +230,11 @@ def get_selected_edge_ids(model: gp.Model, graph: nx.Graph) -> list[int]:
         # for v in sorted(model.getVars(), key=lambda x: x.VarName):
         #     print(f"{v.VarName:<8} = {v.X}")
 
-        for (i,j) in arcs:
+        for (i, j) in arcs:
             x_ij = model.getVarByName(f'x[{i},{j}]')
 
             if x_ij.X == 1:
-                edge_id: int = int(graph.edges[i,j]['id'])
+                edge_id: int = int(graph.edges[i, j]['id'])
                 selected_edges.append(edge_id)
     # else:
     #     for v in sorted(model.getVars(), key=lambda x: x.VarName):
